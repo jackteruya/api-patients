da393c653a6aa2f55a5b1aaf84354f90e1a8d59c
Jackson <jack.teruya@gmail.com>
2024-07-11 15:12:54 -0400

    implement route to create patient

--- src/application/rest/patient.py
+++ src/application/rest/patient.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, HTTPException
 
 from src.application.schemas.patient import PatientCreateEditSchema, PatientSchema, DataPatientsSchema
+from src.domain.use_cases.patients import CreatePatientUseCase
+from src.infra.db.repository import PatientRepository
+from src.infra.db.settings import DBConnectionHandler
 
 patient_route: APIRouter = APIRouter(prefix="/api/v1/patients")
 
@@ -25,10 +28,19 @@ def list_patients():
 
 @patient_route.post('', response_model=PatientSchema)
 def create_patients(patient: PatientCreateEditSchema):
-    data = patient.model_dump()
-    data['id'] = len(list_) + 1
-    list_.append(data)
-    return PatientSchema(**data).model_dump()
+    use_case = CreatePatientUseCase(PatientRepository(DBConnectionHandler))
+    data = use_case.execute(patient)
+    list_.append(data.value)
+    return PatientSchema(
+        id=data.value.id,
+        name=data.value.name,
+        birth_date=data.value.birth_date.strftime('%y-%m-%d'),
+        address=data.value.address,
+        phone=data.value.phone,
+        email=data.value.email,
+        medical_history=data.value.medical_history,
+    ).model_dump()
+    # return data.value
 
 
 @patient_route.put('/{id}', response_model=PatientSchema)
