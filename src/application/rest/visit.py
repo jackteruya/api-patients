081bf865c4d6d34af9ba93772e77922042567e41
Jackson <jack.teruya@gmail.com>
2024-07-11 15:37:26 -0400

    implement route create visit

--- src/application/rest/visit.py
+++ src/application/rest/visit.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, HTTPException
 
 from src.application.schemas import VisitSchema, VisitCreateEditSchema, DataVisitsSchema
+from src.domain.use_cases.visits import CreateVisitUseCase
+from src.infra.db.repository import VisitRepository
+from src.infra.db.settings import DBConnectionHandler
 
 visit_route: APIRouter = APIRouter(prefix="/api/v1/visits")
 
@@ -10,10 +13,14 @@ list_ = []
 
 @visit_route.post('', response_model=VisitSchema)
 def create_visits(visit: VisitCreateEditSchema):
-    data = visit.model_dump()
-    data['id'] = len(list_) + 1
-    list_.append(data)
-    return VisitSchema(**data).model_dump()
+    user_case = CreateVisitUseCase(VisitRepository(DBConnectionHandler))
+    result = user_case.execute(visit)
+    return VisitSchema(
+        id=result.value.id,
+        patient_id=result.value.patient_id,
+        visit_date=result.value.visit_date,
+        summary=result.value.summary,
+    ).model_dump()
 
 
 @visit_route.get('', response_model=DataVisitsSchema)
