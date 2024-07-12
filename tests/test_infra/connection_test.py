07c30ac85da4480d98b21ffbbbe1f4f38b11e378
Jackson <jack.teruya@gmail.com>
2024-07-12 07:53:50 -0400

    add test connection db

---
+++ tests/test_infra/connection_test.py
@@ -0,0 +1,10 @@
+import pytest
+from src.infra.db.settings import DBConnectionHandler
+
+
+@pytest.mark.skip(reason="Sensive test")
+def test_create_database_engine():
+    db_connection_handle = DBConnectionHandler()
+    engine = db_connection_handle.get_engine()
+
+    assert engine is not None
